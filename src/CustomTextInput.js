13988024882f12383bc5a0bfa5e0e0f0a8ffee19
Rachelin <RACHELIN@USERS.NOREPLY.GITHUB.COM>
2016-10-27 18:17:55 +0800

    changed the todo list and add two new components

---
+++ src/CustomTextInput.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+class CustomTextInput extends Component {
+  constructor(props) {
+    super(props);
+    this.focus = this.focus.bind(this);
+  }
+
+  focus() {
+    this.textInput.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          ref={(input) => this.textInput = input} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focus}
+        />
+      </div>
+    );
+  }
+}
+
+export default CustomTextInput;
